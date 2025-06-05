4fb9063e19bce29e8ae68d7fe07e8e5424b7d490
Maksimka37623 <maxsavkiv@gmail.com>
2025-06-05 22:29:35 +0300

    Delete lab 3 directory

--- lab 3/output.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCTION_TABLE_H
-#define FUNCTION_TABLE_H
-
-typedef double (*MathFunc)(double, int);
-double squareRoot(double x, int n); 
-void PrintFunction(double start, double end, double step, int n, MathFunc func);
-
-#endif
